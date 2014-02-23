9ca0deef1eb291d63d535d6b1145be43f5dcc969
davidmcchi <davidchi2015@u.northwestern.edu>
2014-02-23 14:23:42 -0600

    Update NanoWireConstruct3D.py
    added lines for cyl 2-3 and spheres A&B

--- NanoWireConstruct3D.py
+++ NanoWireConstruct3D.py
@@ -23,62 +23,31 @@ lEllipsoid = 150.0
 lWire = (CellY - lSplit)*0.5
 
 #cylinder1
-
 Cyl1= cylinder()
 Cyl1.center = [0,((lSplit+lWire)/2,0]
 Cyl1.height = (CellX-lSplit)/2
 
 #cylinder3
 Cyl3 = cylinder()
+Cly3.center = [.5wSplit+wWire,0,0]
 Cyl3.height = lGap
 
 #cylinder2
 Cyl2 = cylinder()
-Cyl2.center = 
-Cyl2.axis = [x-y for x,y zip(Cyl1.center,Cyl3.center)] #Move centers to end of cylinder 1 and 3
+Cyl2.center = [(.5wSplit+wWire)/2,.5((lSplit+lwWire)/2)
+Cly2.height = lGap
+Cyl2.axis = [x-y for x,y zip(Cyl1.center,Cyl3.center)] 
+#Move centers to end of cylinder 1 and 3
 
+#sphereA
+SphA = sphere()
+SphA.center = [0,lSplit/2,0]
+SphA.radius = .5wWire
+
+#sphereB
+SphB = sphere()
+SphB.center = [.5wSplit+wWire,.5lSplit,0]
+SphB.radius = SphA.radius
 
 radius of connectors is same. same as radius of wire
 
-
-
-
-
-
-
-#topwirecoordinates
-lWire = (CellY - lSplit)*0.5
-topwirecenter = [0,(lSplit+ lWire)/2.0,0]
-hWire = wWire
-topwiresize = [wWire,lWire,hWire]
-
-#bottomwirecoordinates
-bottomwirecenter = [topwirecenter[0],-1*topwirecenter[1],topwirecenter[2]]
-bottomwiresize = topwiresize
-
-#topconecoordinates
-topconeheight = (lSplit-lGap)*0.5
-topconecenter = [0, 0.5*(lGap+topconeheight),0]
-topconetopradius = wWire/2
-topconebottomradius = wSplit/2
-axis = [0,1,0]
-
-#bottomconecoordinates
-bottomconeheight = topconeheight
-bottomconecenter = [topconecenter[0],-1*topconecenter[1],topconecenter[0]]
-bottomconetopradius = topconetopradius
-bottomconebottomradius = topconebottomradius
-axis = [0,-1,0]
-
-#center cylinder coordinates
-radius = wSplit*0.5 
-height = lWire
-axis = [0,1,0]
-
-#ellipsoidcoordinates
-ellipsoidcenter = [0,0,0]
-ellipsoidsize = [(wSplit-2*wWire),lEllipsoid,(hSplit-2*wWire)]
-
-
-
- 
