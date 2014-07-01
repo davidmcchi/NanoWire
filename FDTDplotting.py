862d67e489a58b1ee37b7651923315758da4c41d
jeszekely <jeszekely@gmail.com>
2014-07-01 09:50:14 -0500

    Update FDTDplotting.py

--- FDTDplotting.py
+++ FDTDplotting.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.genfromtxt("!!!filename!!!",usecols=(0))
+nx = max(x)+1
+
+#Same for y column
+
+z = np.zeros((nx,ny),dtype=float)
+datafile = open("!!!filename!!!","r")
